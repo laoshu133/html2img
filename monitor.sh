5831c95c2e960ee5f1226501cf4bc8a50c43eef5
yuyang <yuyang@huanleguang.com>
2015-11-09 10:43:42 +0800

    修改监控脚本和git忽略文件

--- monitor.sh
+++ monitor.sh
@@ -4,5 +4,5 @@ if [ $clipServerNum -lt 1 ]; then
 	if [ $phantomNum -gt 0 ]; then
 		`kill \`ps -fe | grep phantomjs | grep -v grep | awk '{print $2}'\``
 	fi
-	`nohup node /service/html2img/server.js >/dev/null 2>&1 &`
+	`nohup /usr/local/node/bin/node /service/html2img/server.js >/service/html2img/server 2>&1 &`
 fi
