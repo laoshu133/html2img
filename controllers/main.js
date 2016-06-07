353ab9d8e87a9629bea2a08c59d846554c73d845
xiaomi <xiaomi@huanleguang.com>
2016-06-07 22:57:25 +0800

    guide

--- controllers/main.js
+++ controllers/main.js
@@ -7,11 +7,14 @@
 const path = require('path');
 const lodash = require('lodash');
 const send = require('koa-send');
+const fs = require('fs-extra-promise');
 
 const config = require('../services/config');
 const actions = require('../actions/index');
 
 module.exports = function(router) {
+
+    let readmeTpl = '<!DOCTYPE html><html><head><meta charset="UTF-8"><title>Readme</title></head><body><pre>{{content}}</pre></body></html>';
 
     let pathToUrl = function(localPath) {
         let env = process.env;
@@ -29,6 +32,19 @@ module.exports = function(router) {
         let body = this.request.body;
         let query = this.query;
 
+        // Guide
+        if(this.method === 'GET' && lodash.isEmpty(query)) {
+            let readmePath = __dirname + '/../README.md';
+            let readme = yield fs.readFileAsync(readmePath);
+
+            readme = readmeTpl.replace('{{content}}', readme.toString());
+
+            this.body = readme;
+
+            return;
+        }
+
+        // parse config
         let cfg = yield config.create(lodash.merge(query, body));
         if(cfg.dataType === 'image') {
             cfg.wrapMaxCount = 1;
