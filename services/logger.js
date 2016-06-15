5ab2d98676bcbdea17fcbf21102f4f55d08dc1ef
xiaomi <xiaomi@huanleguang.com>
2016-06-15 22:16:44 +0800

    logger - 非 TTY 输出时，格式化时间为当地时间

--- services/logger.js
+++ services/logger.js
@@ -23,4 +23,30 @@ logger.log.log = console.log.bind(console);
 logger.info.log = console.info.bind(console);
 logger.error.log = console.error.bind(console);
 
+// debug.formatArgs
+// 非 TTY 输出时，格式化时间为当地时间
+if(!process.stdout.isTTY) {
+    let rSingleNum = /\b(\d)\b/g;
+    debug.formatArgs = function() {
+        let args = arguments;
+        let name = this.namespace;
+
+        let now = new Date();
+        let timeStr = [
+            now.getFullYear() + '/',
+            now.getMonth() + 1 + '/',
+            now.getDate() + ' ',
+            now.getHours() + ':',
+            now.getMinutes() + ':',
+            now.getSeconds()
+        ]
+        .join('')
+        .replace(rSingleNum, '0$1');
+
+        args[0] = timeStr +' '+ name +' '+ args[0];
+
+        return args;
+    };
+}
+
 module.exports = logger;
