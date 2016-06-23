2b468fe2530a0c5f5b17ec0b067bb96fb4c40ca8
xiaomi <xiaomi@huanleguang.com>
2016-06-23 12:21:32 +0800

    makeshot support cleanTimeoutShots

--- actions/makeshot.js
+++ actions/makeshot.js
@@ -13,30 +13,7 @@ const phantom = require('../lib/phantom');
 const logger = require('../services/logger');
 const config = require('../services/config');
 
-// status counts
-makeshot.shotCounts = {
-    total: 0,
-    success: 0,
-    error: 0
-};
-
-// sync status
-makeshot.syncStatus = function() {
-    let filename = process.pid + '.json';
-    let statusPath = path.join(process.env.STATUS_PATH, filename);
-
-    return phantom.getStatus()
-    .then(statusData => {
-        let status = lodash.assign({
-            shotCounts: makeshot.shotCounts
-        }, statusData);
-
-        return status;
-    })
-    .then(status => {
-        return fs.outputJSONAsync(statusPath, status);
-    });
-};
+const OUT_PATH = process.env.OUT_PATH;
 
 function makeshot(cfg, hooks) {
     logger.info('Actions.makeshot['+ cfg.action +']');
@@ -205,6 +182,72 @@ function makeshot(cfg, hooks) {
         }
     });
 
+};
+
+// status counts
+makeshot.shotCounts = {
+    total: 0,
+    success: 0,
+    error: 0
+};
+
+// sync status
+makeshot.syncStatus = function() {
+    let filename = process.pid + '.json';
+    let statusPath = path.join(process.env.STATUS_PATH, filename);
+
+    return phantom.getStatus()
+    .then(statusData => {
+        let status = lodash.assign({
+            shotCounts: makeshot.shotCounts
+        }, statusData);
+
+        return status;
+    })
+    .then(status => {
+        return fs.outputJSONAsync(statusPath, status);
+    });
+};
+
+// cleanTimeoutShots
+// 删除已超时截图，默认 1 小时超时
+makeshot.cleanTimeoutShots = function() {
+    let now = Date.now();
+    let rOutId = /^[a-z]+\_\d+/i;
+    let shotTimeout = process.env.SHOT_TIMEOUT || 60 * 60 * 1000;
+
+    return fs.readdirAsync(OUT_PATH)
+    .filter(dirname => {
+        if(!rOutId.test(dirname)) {
+            return false;
+        }
+
+        let dirPath = path.join(OUT_PATH, dirname);
+
+        return fs.statAsync(dirPath)
+        .then(stats => {
+            let elapsed = now - stats.mtime.getTime();
+
+            if(
+                stats.isDirectory() &&
+                elapsed > shotTimeout
+            ) {
+                return true;
+            }
+
+            return false;
+        });
+    })
+    .map(dirname => {
+        let dirPath = path.join(OUT_PATH, dirname);
+
+        return fs.removeAsync(dirPath)
+        .then(() => {
+            return dirname;
+        });
+    }, {
+        concurrency: 5
+    });
 };
 
 module.exports = makeshot;
--- controllers/clean.js
+++ controllers/clean.js
@@ -7,11 +7,11 @@
 const path = require('path');
 const fs = require('fs-extra-promise');
 
+const makeshot = require('../actions/makeshot');
+
 const OUT_PATH = process.env.OUT_PATH;
 
 module.exports = function(router) {
-    let resultTimeout = 60 * 60 * 1000;
-
     router.get('/clean', function *() {
         let body = this.request.body;
         let query = this.query;
@@ -20,9 +20,9 @@ module.exports = function(router) {
 
         // 指定 id 删除
         if(id) {
-            let dirpath = path.join(OUT_PATH, id);
+            let dirPath = path.join(OUT_PATH, id);
 
-            yield fs.removeAsync(dirpath);
+            yield fs.removeAsync(dirPath);
 
             this.body = {
                 status: 'success',
@@ -33,42 +33,7 @@ module.exports = function(router) {
         }
 
         // 超时删除
-        let now = Date.now();
-        let rOutId = /^[a-z]+\_\d+/i;
-
-        let removedIds = [];
-
-        yield fs.readdirAsync(OUT_PATH)
-        .filter(dirname => {
-            if(!rOutId.test(dirname)) {
-                return false;
-            }
-
-            let dirpath = path.join(OUT_PATH, dirname);
-
-            return fs.statAsync(dirpath)
-            .then(stats => {
-                let elapsed = now - stats.mtime.getTime();
-
-                if(
-                    stats.isDirectory() &&
-                    elapsed > resultTimeout
-                ) {
-                    return true;
-                }
-
-                return false;
-            });
-        })
-        .map(dirname => {
-            removedIds.push(dirname);
-
-            let dirpath = path.join(OUT_PATH, dirname);
-
-            return fs.removeAsync(dirpath);
-        }, {
-            concurrency: 5
-        });
+        let removedIds = yield makeshot.cleanTimeoutShots();
 
         this.body = {
             status: 'success',
