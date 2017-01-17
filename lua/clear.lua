5dd9615f0407a08aacca06f72f4303223264436d
yuyang <yuyang@huanleguang.com>
2017-01-17 18:15:10 +0800

    修改清理脚本

--- lua/clear.lua
+++ lua/clear.lua
@@ -4,7 +4,7 @@ local curTimestamp = os.time()
 local imgDirPrefix = os.date("%Y%m%d",curTimestamp)
 local curDateHour = os.date("%H")
 local imgDir
-for i=0,tonumber(curDateHour),1 do
+for i=0,tonumber(curDateHour)-1,1 do
 	if i < 10 then
 		imgDir = imgDirPrefix.."0"..i
 	else
