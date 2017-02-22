7c577c34389da430e62b32b6b64009dfd0ee0bca
xiaomi <xiaomi@huanleguang.com>
2017-02-22 14:11:10 +0800

    优化日志打印

--- services/logger.js
+++ services/logger.js
@@ -3,47 +3,42 @@
  *
  * 默认输出至控制台，建议基于 pm2 管理日志
  */
-'use strict';
 
 const debug = require('debug');
 
-let prefix = process.env.DEBUG.replace(/\:\*$/, ':');
+const APP_NAME = process.env.DEBUG.replace(/:\*$/, '');
 
-let logger = {
-    log: debug(prefix + 'log'),
-    info: debug(prefix + 'info'),
-    error: debug(prefix + 'error')
-};
+const logger = require('hlg-logger').create({
+    logServer: process.env.LOG_SERVER,
+    app: APP_NAME
+});
 
-// logger.debug -> logger.log
-logger.debug = logger.log;
-
-logger.log.log = console.log.bind(console);
-logger.info.log = console.info.bind(console);
-logger.error.log = console.error.bind(console);
 
 // debug.formatArgs
 // 非 TTY 输出时，格式化时间为当地时间
 if(!process.stdout.isTTY) {
-    let rSingleNum = /\b(\d)\b/g;
+    // let rSingleNum = /\b(\d)\b/g;
 
     debug.formatArgs = function() {
         let args = arguments;
         let name = this.namespace;
 
-        let now = new Date();
-        let timeStr = [
-            now.getFullYear() + '/',
-            now.getMonth() + 1 + '/',
-            now.getDate() + ' ',
-            now.getHours() + ':',
-            now.getMinutes() + ':',
-            now.getSeconds()
-        ]
-        .join('')
-        .replace(rSingleNum, '0$1');
+        // let now = new Date();
+        // let timeStr = [
+        //     now.getFullYear() + '/',
+        //     now.getMonth() + 1 + '/',
+        //     now.getDate() + ' ',
+        //     now.getHours() + ':',
+        //     now.getMinutes() + ':',
+        //     now.getSeconds()
+        // ]
+        // .join('')
+        // .replace(rSingleNum, '0$1');
 
-        args[0] = timeStr +' '+ name +' '+ args[0];
+        // args[0] = timeStr +' '+ name +' '+ args[0];
+
+        // 取消内部格式化时间，使用 PM2 时间戳
+        args[0] = name + ' ' + args[0];
 
         return args;
     };
